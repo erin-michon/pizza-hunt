bace65cf4339c7d6db63f7ffde66a28efe439c63
Erin M <erin.michon@gmail.com>
2022-02-03 12:30:45 -0500

    added all reply functionality

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
 const {
     addComment,
-    removeComment
+    removeComment,
+    addReply,
+    removeReply,
 } = require('../../controllers/comment-controller');
 
 //set up POST at /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
-//set up DELETE at /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+//set up DELETE and PUT at /api/comments/<pizzaId>/<commentId>
+router.route('/:pizzaId/:commentId')
+.put(addReply)
+.delete(removeComment);
+
+//set up a DELETE at /api/comments/<pizzaId>/<commentId>/<replyId>
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
